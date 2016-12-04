6194cdf477ca3439e66551c90005295de26ef545
Bule-Zst <1583245158@qq.com>
2016-12-04 09:54:42 +0800

    Fixed by Bule-Zst
    change the database name

--- indexPHP.php
+++ indexPHP.php
@@ -3,7 +3,7 @@
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$con = mysqli_connect( "localhost", "root", "" );
-	mysqli_select_db( $con, "yellow_bike" );
+	mysqli_select_db( $con, "zst_yellow_bike" );
 	mysqli_set_charset( $con, "utf8" );
 	$sql = "select password from register where username = '$username'";
 	$result = mysqli_query( $con, $sql );
--- informationPHP.php
+++ informationPHP.php
@@ -2,7 +2,7 @@
 	$bikeNumber = $_POST['bikeNumber'];
 	$password = $_POST['password'];
 	$con = mysqli_connect( "localhost", "root", "" );
-	mysqli_select_db( $con, "yellow_bike" );
+	mysqli_select_db( $con, "zst_yellow_bike" );
 	mysqli_set_charset( $con, "utf8" );
 	$sql = "INSERT INTO information (bikeNumber, password) VALUES ('$bikeNumber', '$password')";
 	if( mysqli_query( $con, $sql )){
